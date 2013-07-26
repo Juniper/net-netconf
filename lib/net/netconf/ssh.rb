25ef54d87d01cf1d99bacbdc18e3f6dad08e07f2
Jeremy Schulman <jeremy@dark-matter.co>
2013-07-26 19:47:44 -0400

    ssh_login enhancements

--- lib/net/netconf/ssh.rb
+++ lib/net/netconf/ssh.rb
@@ -8,8 +8,16 @@ module Netconf
     
     def initialize( args_h, &block )      
       @args = args_h.clone
-      @trans = Hash.new   
+      @args[:os_type] = args_h[:os_type] || Netconf::DEFAULT_OS_TYPE
       
+      # extend this instance with the capabilities of the specific os_type
+      begin
+        extend Netconf::const_get( @args[:os_type] )::TransSSH        
+      rescue NameError
+        # no extensions available ...
+      end     
+      
+      @trans = Hash.new   
       super( &block )
     end
     
@@ -20,9 +28,17 @@ module Netconf
       start_args[:passphrase] = @args[:passphrase] || nil
       start_args[:port] = @args[:port] || NETCONF_PORT
       start_args.merge!(@args[:ssh_args]) if @args[:ssh_args]
-      
-      @trans[:conn] = Net::SSH.start( @args[:target], @args[:username], start_args )     
-      @trans[:chan] = @trans[:conn].open_channel{ |ch| ch.subsystem( NETCONF_SUBSYSTEM ) }      
+            
+      begin
+        @trans[:conn] = Net::SSH.start( @args[:target], @args[:username], start_args )     
+        @trans[:chan] = @trans[:conn].open_channel{ |ch| ch.subsystem( NETCONF_SUBSYSTEM ) }
+      rescue Errno::ECONNREFUSED => e
+        if self.respond_to? 'trans_on_connect_refused'
+          return trans_on_connect_refused( start_args )
+        end
+        return nil
+      end
+      @trans[:chan]
     end
     
     def trans_close     
