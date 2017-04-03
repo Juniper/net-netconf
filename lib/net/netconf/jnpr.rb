f018f27dab50740a0030b5d785d071320f6ecbbb
David Gethings <dgethings@juniper.net>
2017-04-03 16:18:29 +0100

    done jnpr.rb

--- lib/net/netconf/jnpr.rb
+++ lib/net/netconf/jnpr.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'net/netconf'
 require 'net/netconf/jnpr/rpc'
 require 'net/netconf/jnpr/junos_config'
 
-module Netconf::Junos
-  NETCONF_CLI = "junoscript netconf need-trailer"
-  NETCONF_SHELL = "exec xml-mode netconf need-trailer"
+module Netconf
+  module Junos
+    NETCONF_CLI = 'junoscript netconf need-trailer'
+    NETCONF_SHELL = 'exec xml-mode netconf need-trailer'
+  end
 end
-  
