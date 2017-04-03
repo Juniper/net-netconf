37072477a8f2623aff94776d976044e1cf672dd4
David Gethings <dgethings@juniper.net>
2017-04-03 18:19:38 +0100

    done jnpr/ssh.rb

--- lib/net/netconf/jnpr/ssh.rb
+++ lib/net/netconf/jnpr/ssh.rb
@@ -1,27 +1,25 @@
+# frozen_string_literal: true
+
 require 'net/netconf'
 require 'net/netconf/jnpr'
 
 module Netconf
-  module Junos  
+  module Junos
+    # this is used to handle the case where NETCONF (port 830) is disabled.
+    # We can still access the NETCONF subsystem from the CLI using a hidden
+    # command 'netconf'
     module TransSSH
-    
-      ##
-      ## this is used to handle the case where NETCONF (port 830) is disabled.  We can still access
-      ## the NETCONF subsystem from the CLI using a hidden command 'netconf'
-      ##
-      
-      def trans_on_connect_refused( start_args )        
-        start_args[:port] = 22      
-        @trans[:conn] = Net::SSH.start( @args[:target], @args[:username], start_args )
+      def trans_on_connect_refused(start_args)
+        start_args[:port] = 22
+        @trans[:conn] = Net::SSH.start(@args[:target], @args[:username], start_args)
         do_once = true
-        @trans[:conn].exec( NETCONF_CLI ) do |chan, success|        
+        @trans[:conn].exec(NETCONF_CLI) do |chan, _success|
           @trans[:chan] = chan
           do_once = false
         end
-        @trans[:conn].loop { do_once }   
+        @trans[:conn].loop { do_once }
         @trans[:chan]
       end
-      
     end
-  end  
-end    
+  end
+end
