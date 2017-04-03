4eec0e680764fa4970b154956db434f81213162d
David Gethings <dgethings@juniper.net>
2017-04-03 18:16:17 +0100

    done jnpr/serial.rb

--- lib/net/netconf/jnpr/serial.rb
+++ lib/net/netconf/jnpr/serial.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require 'net/netconf'
 require 'net/netconf/serial'
 require 'net/netconf/jnpr'
 
 module Netconf
-  module Junos    
-    module TransSerial      
-      def trans_start_netconf( last_console )
+  module Junos
+    module TransSerial
+      def trans_start_netconf(last_console)
         last_console.match(/[^%]\s+$/)
-        netconf_cmd = ($1 == '%') ? Netconf::Junos::NETCONF_SHELL : Netconf::Junos::NETCONF_CLI
+        netconf_cmd = $1 == '%' ? Netconf::Junos::NETCONF_SHELL : Netconf::Junos::NETCONF_CLI
         puts netconf_cmd
-      end      
-    end      
+      end
+    end
   end
 end
