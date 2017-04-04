04dad47ccc53179526bfd9f8600088fa1afcd5c9
David Gethings <dgethings@juniper.net>
2017-04-04 12:57:16 +0100

    fixed jnpr scp.rb example

--- examples/jnpr/scp.rb
+++ examples/jnpr/scp.rb
@@ -1,22 +1,24 @@
 require 'net/netconf'
 require 'net/scp'
 
-login = { :target => 'vsrx', :username => "jeremy", :password => "jeremy1" }
-
-file_name = __FILE__  
-
-Netconf::SSH.new( login ){ |dev|  
-
-  inv = dev.rpc.get_chassis_inventory  
-
-  puts "Chassis: " + inv.xpath('chassis/description').text
-  puts "Chassis Serial-Number: " + inv.xpath('chassis/serial-number').text  
-    
-  puts "Copying file #{file_name} to home directory ..."  
-  dev.scp.upload! file_name, file_name
- 
-  puts "Copying latest config file from target to local machine ..."
-  
-  dev.scp.download! "/config/juniper.conf.gz", "/var/tmp/config.gz"
+login = {
+  target: 'vsrx',
+  username: 'jeremy',
+  password: 'jeremy1'
 }
 
+file_name = __FILE__
+
+Netconf::SSH.new(login) do |dev|
+  inv = dev.rpc.get_chassis_inventory
+
+  puts 'Chassis: ' + inv.xpath('chassis/description').text
+  puts 'Chassis Serial-Number: ' + inv.xpath('chassis/serial-number').text
+
+  puts "Copying file #{file_name} to home directory ..."
+  dev.scp.upload! file_name, file_name
+
+  puts 'Copying latest config file from target to local machine ...'
+
+  dev.scp.download! '/config/juniper.conf.gz', '/var/tmp/config.gz'
+end
